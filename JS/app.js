a9ff36c8bbfd31f324aa2658bfb92cf44aa15abb
Abdalla-Hirzalla <a.hirzalla@hotmail.com>
2021-06-13 13:40:28 +0300

    add product page js

---
+++ JS/app.js
@@ -0,0 +1,16 @@
+'use strict';
+function showPreview(){
+let htmlCode = document.getElementById('htmlcode').value
+
+let cssCode = "<style>"+ document.getElementById('csscode').value + "</style>";
+
+let jsCode = "<scri"+"pt>" + document.getElementById('jscode').value + "</scri"+"pt>" ;
+
+let frame = document.getElementById('preview-window').contentWindow.document ;
+
+frame.open();
+document.getElementById('button1').onclick = function (){frame.write(htmlCode+cssCode+jsCode);};
+
+frame.close();
+}
+
