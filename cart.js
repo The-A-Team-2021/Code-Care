253f45bb810796c515db9b0cc1f5e19563706b0a
Manal Ateyyat <manalateyyat4@gmail.com>
2021-06-14 16:29:04 +0300

    JS Cart

--- cart.js
+++ cart.js
@@ -7,6 +7,8 @@ let form = document.getElementById('form');
 
 
 
+
+
 function Customer(name,phone,email) {
     this.name=name;
     this.phone=phone;
@@ -14,153 +16,173 @@ function Customer(name,phone,email) {
     
     Customer.customerArray.push(this);
     
+    
+    
 }
-
 Customer.customerArray=[];
+
 
 form.addEventListener('submit', submitter);
 
 
-
 function submitter(event) {
-    event.preventDefault();
+   event.preventDefault();
     
-    Customer.customerArray.name=event.target.name.value;
+    let name= event.target.name.value;
+
+    let phone =event.target.phoneNumber.value;
+   
+    let email=event.target.email.value;
+
+    new Customer(name, phone,email);
+
+    alert(`Thank you ${name} we will send you confirmation Message to: ${email}`);
     
-    Customer.customerArray.phone=event.target.phoneNumber.value;
-    
-    Customer.customerArray.email=event.target.email.value;
-
-
-    // console.log( Customer.customerArray.name);
-    // console.log(Customer.customerArray.phone);
-    // console.log( Customer.customerArray.email);
-
     console.log(Customer.customerArray);
-
-
+    
     
     storeCustomerData();
 }
 
-// let profile;
-
-// for (let i = 0; i < Customer.customerArray.length; i++) {
-    
-//     profile.
-    
-// }
-
-// let data;
-
-// console.log(Customer.customerArray);
 
 
-function storeCustomerData () {
-    
-    let data=JSON.stringify(Customer.customerArray);
 
-    localStorage.setItem('Customer',data);
 
-    
-    
-    
-    
-    console.log(data);
+
+//let cartItems=[];
+function CartItems(name,price,quantity) {
+  this.name=name;
+  this.price=price;
+  this.quantity=quantity;
+  
+  CartItems.cartArray.push(this);
+  
+  
+  
 }
-
-
-
-
-
-
-// let data;
-// function addProductToLocalStorage()
-// {
-//   localStorage.setItem('Items',data) ;
-//   data=JSON.stringify(cart);
-
-//   console.log(cart);
-
-// }
-
-
-
-
-
-
-
-
-
-let cartItems;
+CartItems.cartArray=[];
 
 
 function getFromLocalStorage () {
 
-  cartItems = JSON.parse(localStorage.getItem('Items'));
+  CartItems.cartArray = JSON.parse(localStorage.getItem('Cart'));
 
-  console.log(cartItems);
+  console.log(CartItems.cartArray);
+  //console.log(CartItems.cartArray[0])
+
+//   for (let i = 0; i <CartItems.cartArray.length; i++) {
+  
+  
+//     cartItems[i]= new CartItems();
+
+   
+//    }
+// console.log(`new array ${cartItems}`)
+  
+
 }
 
+console.log(CartItems.cartArray);
 
-
+let totalPrice=0;
 function render () {
-  for (let i = 0; i < cartItems.length ; i++) {
-    let trProducts = document.createElement('tr');
 
+ // console.log(`cart ${cartItems}`)
+
+  for (let i = 0; i < CartItems.cartArray .length ; i++) {
+    let trProducts = document.createElement('tr');
+    trProducts.setAttribute('id', 'trProducts'+i)
     table.appendChild(trProducts);
 
     let tdProducts = document.createElement('td');
+   
     trProducts.appendChild(tdProducts);
 
-    tdProducts.textContent = cartItems[i] ;
+    tdProducts.textContent = CartItems.cartArray[i].name ;
 
 
     let tdPrice = document.createElement('td');
     trProducts.appendChild(tdPrice);
 
-    tdPrice.textContent = '50jd';
+    tdPrice.textContent = CartItems.cartArray[i].price;
+    totalPrice+=CartItems.cartArray[i].price;
+    
+    let tdQuan = document.createElement('td');
+    trProducts.appendChild(tdQuan);
 
+    tdQuan.textContent = CartItems.cartArray[i].quantity;
 
     let tdRemove = document.createElement('td');
+   
+    tdRemove.setAttribute('id', 'tdRemove'+i)
+    
     trProducts.appendChild(tdRemove);
+
 
     tdRemove.textContent = 'X' ;
   }
 
+  let total=document.createElement('tr');
+  
+  table.appendChild( total);
+
+  let tdtotal = document.createElement('td');
+  total.appendChild(tdtotal );
+
+   tdtotal.textContent = 'Total' ;
 
 
+    let tdTotalPrice = document.createElement('td');
+    total.appendChild(tdTotalPrice);
 
+    tdTotalPrice.textContent = totalPrice;
 
-}
-
-
-table.addEventListener('click', deleteItem);
-
-
-
-function deleteItem(event) {
-
-  if (event.target.textContent === 'X' ) {
-    // cartItems.removeItem(event.target.id);
-    // localStorage.deleteRow();
-    // localStorage.removeItem(event.target.id);
-
-    console.log(event);
+    
     
 
-  }
-
-
- 
 }
 
+table.addEventListener('click',removeItem);
+
+function removeItem(event){
+     event.preventDefault();
+
+    // console.log(event)
 
 
+  if (event.target.textContent === 'X')
+  {
+    //console.log(event.target.id)
+   let x= event.target.id
+
+   //console.log(x.length-1)
+   let y=x[x.length-1];
+   let z="trProducts"
+
+    z+=y 
+    //console.log(z)
+    let tr=document.getElementById(`${z}`);
+    tr.textContent='';
+  
+   console.log(`remove${CartItems.cartArray[0]}`)
+    //
+    // table.removeChild(`${z}`)
+  
+    //table.textContent='';
+    //totalPrice=0;
+    //render();
+
+   
+  }
+  
+}
 
 
 getFromLocalStorage();
 
 render();
+
+
+
 
 
