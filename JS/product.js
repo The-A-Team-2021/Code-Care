eaa909fc9e86ed7ead4631a2812c5134b7d09351
hatemsallam <hatemsallam94@hotmail.com>
2021-06-16 21:52:34 +0300

    finalcss3

--- JS/product.js
+++ JS/product.js
@@ -84,12 +84,22 @@ function getButton(event) {
                 Product.allProducts[i].quantity++;
 
                 Product.allProducts[i].totalPrice+=Product.allProducts[i].price
+                Swal.fire(
+                    'Added to Cart!',
+                    'Your Product Successfuly added to the cart!',
+                    'success'
+                  )
 
             }
             else {
 
                 cart.push(Product.allProducts[i]);
                 Product.allProducts[i].quantity++;
+                Swal.fire(
+                    'Added to Cart!',
+                    'Your Product Successfuly added to the cart!',
+                    'success'
+                  )
 
 
             }
